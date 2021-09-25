6a9b73abd3ad55cc764e6de4ec7697536d5d3203
Aditya Pratap <adityapratap7344@gmail.com>
2021-09-25 22:39:02 +0530

    📦 NEW: setup links

--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,31 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import Home from '../views/Home.vue';
 import About from '../views/About.vue';
+import Manage from '../views/Manage.vue';
 
 /** @type {import('vue-router').RouteRecordRaw[]} */
 const routes = [
   {
+    name: 'home',
     path: '/',
     component: Home,
   },
   {
+    name: 'about',
     path: '/about',
     component: About,
+  },
+  {
+    name: 'manage',
+    path: '/manage',
+    component: Manage,
   },
 ];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes,
+  linkExactActiveClass: 'text-green-500',
 });
 
 export default router;
