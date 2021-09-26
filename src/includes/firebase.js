d32808e5fce1577c69c470d828f0b88d32bb115a
Aditya Pratap <adityapratap7344@gmail.com>
2021-09-26 19:01:16 +0530

    📦 NEW: added song upload feature

--- src/includes/firebase.js
+++ src/includes/firebase.js
@@ -1,6 +1,7 @@
 import firebase from 'firebase/app';
 import 'firebase/auth';
 import 'firebase/firestore';
+import 'firebase/storage';
 
 const firebaseConfig = {
   apiKey: 'AIzaSyDkDfoIJEiIp313imKyF4OcbfRjXMfx26M',
@@ -19,3 +20,5 @@ export const firebaseAuth = firebase.auth();
 export const firestore = firebase.firestore();
 
 export const usersCollection = firestore.collection('users');
+
+export const firebaseStorage = firebase.storage();
