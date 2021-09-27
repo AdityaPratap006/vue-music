b38aa5af72b7c3714022c613c9c8ef2efccfddd0
Aditya Pratap <adityapratap7344@gmail.com>
2021-09-27 22:36:16 +0530

    📦 NEW: Rendering songs uploaded by the user

---
+++ src/components/CompositionItem.vue
@@ -0,0 +1,107 @@
+<template>
+  <div class="border border-gray-200 p-3 mb-4 rounded">
+    <div v-show="!showForm">
+      <h4 class="inline-block text-2xl font-bold">
+        {{ song.modifiedName }}
+      </h4>
+      <button class="ml-1 py-1 px-2 text-sm rounded text-white bg-red-600 float-right">
+        <i class="fa fa-times"></i>
+      </button>
+      <button
+        class="ml-1 py-1 px-2 text-sm rounded text-white bg-blue-600 float-right"
+        @click.prevent="toggleForm"
+      >
+        <i class="fa fa-pencil-alt"></i>
+      </button>
+    </div>
+    <div v-show="showForm">
+      <vee-form
+        :validation-schema="schema"
+        :initial-values="{ title: song.modifiedName, genre: song.genre }"
+        @submit="edit"
+      >
+        <div class="mb-3">
+          <label class="inline-block mb-2">Song Title</label>
+          <vee-field
+            name="title"
+            type="text"
+            class="
+              block
+              w-full
+              py-1.5
+              px-3
+              text-gray-800
+              border border-gray-300
+              transition
+              duration-500
+              focus:outline-none focus:border-black
+              rounded
+            "
+            placeholder="Enter Song Title"
+          />
+          <vee-error-message class="text-red-600 text-xs" name="title" />
+        </div>
+        <div class="mb-3">
+          <label class="inline-block mb-2">Genre</label>
+          <vee-field
+            name="genre"
+            type="text"
+            class="
+              block
+              w-full
+              py-1.5
+              px-3
+              text-gray-800
+              border border-gray-300
+              transition
+              duration-500
+              focus:outline-none focus:border-black
+              rounded
+            "
+            placeholder="Enter Genre"
+          />
+          <vee-error-message class="text-red-600 text-xs" name="genre" />
+        </div>
+        <div class="flex justify-start items-center gap-2 mt-6">
+          <button type="submit" class="py-1.5 px-3 rounded text-white bg-green-600">Submit</button>
+          <button
+            type="button"
+            class="py-1.5 px-3 rounded text-white bg-gray-600"
+            @click.prevent="toggleForm"
+          >
+            Go Back
+          </button>
+        </div>
+      </vee-form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CompositionItem',
+  props: {
+    song: {
+      type: Object,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      showForm: false,
+      schema: {
+        title: 'required',
+        genre: 'alpha_spaces',
+      },
+    };
+  },
+  methods: {
+    toggleForm() {
+      this.showForm = !this.showForm;
+    },
+    edit(values) {
+      console.log(values);
+    },
+  },
+};
+</script>
